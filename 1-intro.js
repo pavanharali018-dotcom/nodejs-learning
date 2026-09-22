8cfc85545a5cf406e77b11bbfd87481c87ca4a43
Pavan Harali <pavanharali018@gmail.com>
2026-09-23 01:34:47 +0530

    Globals idea

---
+++ 1-intro.js
@@ -0,0 +1,5 @@
+// Globals are objects/functions/variables that are available throughout a Node.js application without importing them.
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(process);
