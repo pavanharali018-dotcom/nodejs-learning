9d13b2fab371c346b3318fd61c47ef764f4102c4
Pavan Harali <pavanharali018@gmail.com>
2026-09-25 02:05:44 +0530

    created a file using async writefile method

--- 2-modules.js
+++ 2-modules.js
@@ -4,26 +4,26 @@
 //built in modules
 //1.OS module
 // The 'os' module provides information about the operating system and computer.
-const OS = require('os');
-console.log(OS.platform());
+// const OS = require('os');
+// console.log(OS.platform());
 // os.platform() returns the operating system platform on which Node.js is running.
-console.log(OS.homedir());
+// console.log(OS.homedir());
 // os.homedir() returns the home directory of the current user.
-console.log(OS.cpus());
+// console.log(OS.cpus());
 // os.cpus() returns information about the CPU cores available on the system.
-console.log(OS.totalmem());
-console.log(OS.freemem());
+// console.log(OS.totalmem());
+// console.log(OS.freemem());
 // os.totalmem() returns the total amount of system memory in bytes.
 // os.freemem() returns the amount of free system memory in bytes.
 
 //2.Path module
 // The 'path' module provides utilities for working with file and directory paths.
-const path = require("path");
-console.log(path.join("folder","subfolder","file.txt"));
+// const path = require("path");
+// console.log(path.join("folder","subfolder","file.txt"));
 // path.join() → joins different parts of a path into one proper path.
-console.log(path.basename("folder/subfolder/file.txt"));
+// console.log(path.basename("folder/subfolder/file.txt"));
 // path.basename() returns the last part of a path, usually the file name.
-console.log(path.dirname("folder/subfolder/file.txt"));
+// console.log(path.dirname("folder/subfolder/file.txt"));
 // path.dirname() returns the directory part of a path.
 
 
@@ -46,21 +46,29 @@ const fs = require("fs");
 // fs.unlinkSync() deletes a file.
 
 // async fs module
-fs.readFile("demo.txt","utf8",(err,data)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log(data);
+// fs.readFile("demo.txt","utf8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
 
-});
+// });
 
-fs.readFile("docomo.txt","utf8",(err,data)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log(data);
+// fs.readFile("docomo.txt","utf8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
 
-});
+// });
 // In asynchronous file operations, the callback receives an error if the operation fails.
+
+fs.writeFile("async-demo.txt","hello from aync node.js!",(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("written successfully");
+});
